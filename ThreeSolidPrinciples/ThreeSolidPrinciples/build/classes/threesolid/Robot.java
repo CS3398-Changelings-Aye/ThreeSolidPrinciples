799c089dfc4190e6af3b38c238ddc994c22edd24
Dalwy <DalwyXD12@gmail.com>
2019-09-11 00:05:06 -0500

    Updated
    Updated the Robot Class with the Reasoning for Organizing and The Principle used.

--- ThreeSolidPrinciples/ThreeSolidPrinciples/build/classes/threesolid/Robot.java
+++ ThreeSolidPrinciples/ThreeSolidPrinciples/build/classes/threesolid/Robot.java
@@ -1,10 +1,22 @@
 package threesolid;
 
-class Robot implements threesolid.IWorker
+
+/*
+    Name: Dalton Melville
+    NetID: Drm143
+    Reasoning: The reason for reorganizing the code is to enable
+    readability, organization, to make debugging simpler, and to ensure that
+    errors can be found fast and taken care of swiftly.
+    Principles used: The principles used in this file is the Open Close Principle.
+    The Open Close Principle is used to allow the method to be extended with the minimum amount
+    of modification to the methods. 
+ */
+class Robot implements IWorker
 {
     public void work()
     {
         //.. Work goes here
+        System.out.format("OwO I did it!\n");
 
     }
     public void eat()
