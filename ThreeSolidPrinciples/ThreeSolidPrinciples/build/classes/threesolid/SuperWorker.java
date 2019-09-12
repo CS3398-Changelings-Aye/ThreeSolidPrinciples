58fbb4ed4a8ffc15c008a4c240315a808cb69113
wilson21x <43559667+wilson21x@users.noreply.github.com>
2019-09-12 17:15:03 -0500

    Update SuperWorker.java

--- ThreeSolidPrinciples/ThreeSolidPrinciples/build/classes/threesolid/SuperWorker.java
+++ ThreeSolidPrinciples/ThreeSolidPrinciples/build/classes/threesolid/SuperWorker.java
@@ -1,5 +1,14 @@
 package threesolid;
 
+/*
+    Name: Wilson Benitez
+    Net ID: wab57
+    Reasoning: The reason for reorganizing the code is for everything to look
+    nicer and easier to get to when it comes to debugging, its better than
+    looking through at a big bulk of code for whatever reason.
+    Principle: The principle used in the class is Open Close Princple.
+*/
+
 class SuperWorker implements threesolid.IWorker {
     public void work() {
         //.... working much more
