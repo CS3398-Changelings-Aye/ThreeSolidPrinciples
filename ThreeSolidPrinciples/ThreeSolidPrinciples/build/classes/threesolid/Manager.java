13a932025744b509e475199186b8d3dd35c0dd36
nisaltf315 <nisaltf@gmail.com>
2019-09-12 15:03:55 -0500

    Update Manager.java

--- ThreeSolidPrinciples/ThreeSolidPrinciples/build/classes/threesolid/Manager.java
+++ ThreeSolidPrinciples/ThreeSolidPrinciples/build/classes/threesolid/Manager.java
@@ -1,5 +1,14 @@
 package threesolid;
-
+/*
+  Name: Nisa Lateef   
+    NetID: n_l110
+    Reasoning: The reason for reorganizing the code is to make sure a class has
+    one function so debugging/modifying the code is made easier.
+    Principles used: The principle used in this file is Single Responsibility.
+    Single Responsibility is used to split functionality between classes. Each
+    class should handle one responsibility and if a change occurs, than we will
+    make changes in the class that handles that responsibility. 
+    */
 class Manager {
     threesolid.IWorker worker;
 
